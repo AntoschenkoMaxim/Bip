b70c40884d72f8011cf6e2db4622c2a14e71c1b5
Maxim <jurusski@gmail.com>
2023-04-24 01:36:42 +0300

    Initialization client part.

---
+++ bip-client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        {/* <Route path='/' element={<MainLayout />}>
+          <Route index element={<Brands />} />
+        </Route> */}
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
+    </>
+  )
+}
+
+export default App
