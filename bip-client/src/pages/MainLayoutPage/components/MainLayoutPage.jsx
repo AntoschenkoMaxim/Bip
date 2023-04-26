315febb076c5ccb3bdb15fcb58bd85211c021f8c
Maxim <jurusski@gmail.com>
2023-04-26 03:54:51 +0300

    Created MainLayoutPage page.

---
+++ bip-client/src/pages/MainLayoutPage/components/MainLayoutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MainLayout } from '../../../modules/MainLayout'
+
+const MainLayoutPage = () => {
+  return (
+    <>
+      <MainLayout />
+    </>
+  )
+}
+
+export default MainLayoutPage
