d424a4520739792dd0091029b5e61582cd652f89
Maxim <jurusski@gmail.com>
2023-04-28 02:13:07 +0300

    Created Posts page.

---
+++ bip-client/src/pages/Posts/components/Posts.jsx
@@ -0,0 +1,48 @@
+import { Button, Divider, Modal } from 'antd'
+import { useState } from 'react'
+import { PostsTable } from '../../../modules/PostsTable'
+import { CreatePostForm } from '../../../modules/CreatePostForm'
+
+export function Posts() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const showModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleOk = () => {
+    setIsModalOpen(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalOpen(false)
+  }
+
+  return (
+    <>
+      <Button onClick={showModal}>Добавить новость</Button>
+      <Modal
+        title='Создание новости'
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={[
+          <Button key='back' onClick={handleCancel}>
+            Закрыть
+          </Button>,
+          <Button
+            form='create_post_form'
+            key='submit'
+            type='primary'
+            htmlType='submit'
+          >
+            Добавить
+          </Button>,
+        ]}
+      >
+        <CreatePostForm handleOk={handleOk} />
+      </Modal>
+      <Divider />
+      <PostsTable />
+    </>
+  )
+}
