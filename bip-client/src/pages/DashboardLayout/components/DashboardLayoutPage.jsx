c349990dbeafe6011eacee2330854b768482aefc
Maxim <jurusski@gmail.com>
2023-05-11 10:15:59 +0300

    Changed name MainLayout to DashboardLayout.

---
+++ bip-client/src/pages/DashboardLayout/components/DashboardLayoutPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { DashboardLayout } from '../../../modules/DashboardLayout'
+
+export function DashboardLayoutPage() {
+  return (
+    <>
+      <DashboardLayout />
+    </>
+  )
+}
