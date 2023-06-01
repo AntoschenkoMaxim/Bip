b6235e730d5076f586ea00833cdcb33d73a68644
Maxim <jurusski@gmail.com>
2023-06-01 03:58:12 +0300

    Created Regulations module.

---
+++ bip-client/src/modules/Regulations/constants/mockdata.js
@@ -0,0 +1,11 @@
+import * as images from '../../../assets/index'
+
+export const mockdata = [
+  images.regulation1,
+  images.regulation2,
+  images.regulation3,
+  images.regulation4,
+  images.regulation5,
+  images.regulation6,
+  images.regulation7,
+]
