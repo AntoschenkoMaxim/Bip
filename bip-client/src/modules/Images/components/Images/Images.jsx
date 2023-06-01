a2e5d532d1402eff14ecc964e8fb1504a31a1ed0
Maxim <jurusski@gmail.com>
2023-06-01 04:00:11 +0300

    Created Images module.

---
+++ bip-client/src/modules/Images/components/Images/Images.jsx
@@ -0,0 +1,70 @@
+import { Card, Image, List, Tabs } from 'antd'
+import { useEffect, useState } from 'react'
+import { useGetAllImageCategoriesQuery } from '../../../../hooks/useGetAllImageCategories'
+import { useGetAllImagesQuery } from '../../../../hooks/useGetAllImagesQuery'
+const { Meta } = Card
+
+export function Images() {
+  const [id, setId] = useState(null)
+
+  const { data: imageCategories } = useGetAllImageCategoriesQuery()
+
+  const items = imageCategories?.rows.map((item) => ({
+    key: item.id,
+    label: item.description,
+  }))
+
+  const { data: images, isSuccess } = useGetAllImagesQuery()
+
+  useEffect(() => {
+    if (items && !id) {
+      setId(items[0].key)
+    }
+  }, [items, id])
+
+  const getFilteredImages = () => {
+    const filteredImages = images?.rows.filter((item) => item.categoryId === id)
+    return filteredImages
+  }
+
+  const handleChange = (key) => {
+    setId(key)
+  }
+
+  return (
+    <>
+      <Tabs items={items} onChange={handleChange} />
+      {isSuccess && (
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 4,
+            lg: 4,
+            xl: 6,
+            xxl: 3,
+          }}
+          pagination={{
+            pageSize: 6,
+          }}
+          dataSource={getFilteredImages()}
+          renderItem={(item) => (
+            <List.Item>
+              <Card
+                cover={
+                  <Image
+                    src={`${import.meta.env.VITE_BASE_URL}/${item.image}`}
+                    alt={item.image}
+                  />
+                }
+              >
+                <Meta title={item.title} />
+              </Card>
+            </List.Item>
+          )}
+        />
+      )}
+    </>
+  )
+}
---
+++ bip-client/src/modules/Images/index.js
@@ -0,0 +1,1 @@
+export { Images } from './components/Images/Images'
