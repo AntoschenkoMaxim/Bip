c6c0dbff82ad39ca5d512342057b8e4fab9ca584
Maxim <jurusski@gmail.com>
2023-06-16 05:52:38 +0300

    Updated Schedule module.

--- bip-client/src/modules/Schedule/components/Schedule/Schedule.jsx
+++ bip-client/src/modules/Schedule/components/Schedule/Schedule.jsx
@@ -1,6 +1,40 @@
-import { Image } from 'antd'
-import * as images from '../../../../assets/index'
+import { CaretRightOutlined } from '@ant-design/icons'
+import { Collapse, Image, theme } from 'antd'
+import { useGetAllSchedulesQuery } from '../../../../hooks/useGetAllSchedulesQuery'
+const { Panel } = Collapse
 
 export function Schedule() {
-  return <Image src={images.schedule} alt='schedule' />
+  const { token } = theme.useToken()
+
+  const panelStyle = {
+    marginBottom: 24,
+    background: token.colorFillAlter,
+    borderRadius: token.borderRadiusLG,
+    border: 'none',
+  }
+
+  const { data: schedules, isSuccess } = useGetAllSchedulesQuery()
+
+  return (
+    <Collapse
+      bordered={false}
+      expandIcon={({ isActive }) => (
+        <CaretRightOutlined rotate={isActive ? 90 : 0} />
+      )}
+      style={{
+        background: token.colorBgContainer,
+      }}
+    >
+      {isSuccess &&
+        schedules?.rows.map((item) => (
+          <Panel header={item.title} key={item.image} style={panelStyle}>
+            <Image
+              width={400}
+              src={`${import.meta.env.VITE_BASE_URL}/${item.image}`}
+              alt={item.image}
+            />
+          </Panel>
+        ))}
+    </Collapse>
+  )
 }
