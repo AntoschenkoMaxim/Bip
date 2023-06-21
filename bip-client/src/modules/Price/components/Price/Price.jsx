32b4be377263c9881338711708609b8c8a39c757
Maxim <jurusski@gmail.com>
2023-06-21 04:57:37 +0300

    Updated Price module.

--- bip-client/src/modules/Price/components/Price/Price.jsx
+++ bip-client/src/modules/Price/components/Price/Price.jsx
@@ -1,19 +1,48 @@
-import { Image } from 'antd'
+import { CaretRightOutlined } from '@ant-design/icons'
+import { Col, Collapse, Image, Row, theme } from 'antd'
+import { useGetAllSchedulesQuery } from '../../../../hooks/useGetAllSchedulesQuery'
 import { useGetAllPricesQuery } from '../../../../hooks/useGetAllPricesQuery'
+const { Panel } = Collapse
 
 export function Price() {
+  const { token } = theme.useToken()
+
+  const panelStyle = {
+    marginBottom: 24,
+    background: token.colorFillAlter,
+    borderRadius: token.borderRadiusLG,
+    border: 'none',
+  }
+
   const { data: prices, isSuccess } = useGetAllPricesQuery()
 
-  console.log(prices)
-
   return (
-    <>
-      {isSuccess && (
-        <>
-          {/* <Image src={prices?.rows.price_image} />
-          <Image src={prices?.rows.payment_image} /> */}
-        </>
+    <Collapse
+      bordered={false}
+      expandIcon={({ isActive }) => (
+        <CaretRightOutlined rotate={isActive ? 90 : 0} />
       )}
-    </>
+      style={{
+        background: token.colorBgContainer,
+      }}
+    >
+      {isSuccess &&
+        prices?.rows.map((item) => (
+          <Panel header={item.title} key={item.image} style={panelStyle}>
+            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+              <Col>
+                <Image
+                  src={`${import.meta.env.VITE_BASE_URL}/${item.price_image}`}
+                />
+              </Col>
+              <Col>
+                <Image
+                  src={`${import.meta.env.VITE_BASE_URL}/${item.payment_image}`}
+                />
+              </Col>
+            </Row>
+          </Panel>
+        ))}
+    </Collapse>
   )
 }
