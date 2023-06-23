57c267f914c9ddd8d23f88bbdad4f0806011241f
Maxim <jurusski@gmail.com>
2023-06-23 06:12:55 +0300

    Created Landing page.

---
+++ bip-client/src/pages/Landing/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import { Feature } from '../../../modules/Feature'
+import { Hero } from '../../../modules/Hero'
+import { Team } from '../../../modules/Team'
+import { Testimonials } from '../../../modules/Testimonials'
+
+export function LandingPage() {
+  return (
+    <>
+      <Hero />
+      <Feature />
+      <Team />
+      <Testimonials />
+    </>
+  )
+}
